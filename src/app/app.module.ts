216ac1a82adc998910f9d60e35907b5839226783
H8055 <hafaz.faz.1@gamail.com>
2023-01-25 18:00:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RolesComponent } from './components/roles/roles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    NavbarComponent,
+    RolesComponent
   ],
   imports: [
     BrowserModule,
